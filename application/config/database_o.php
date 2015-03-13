ada97640ffc30596dd09948ea0525eb30463c1a0
Juan Martín <jmcataldo@gmail.com>
2015-03-13 15:18:29 -0300

    db config

--- application/config/database_o.php
+++ application/config/database_o.php
@@ -49,8 +49,8 @@ $active_group = 'default';
 $active_record = TRUE;
 
 $db['default']['hostname'] = 'localhost';
-$db['default']['username'] = 'uv0086';
-$db['default']['password'] = 'bieu@2014';
+$db['default']['username'] = '***';
+$db['default']['password'] = '***';
 $db['default']['database'] = 'uv0086_db';
 $db['default']['dbdriver'] = 'mysql';
 $db['default']['dbprefix'] = '';
